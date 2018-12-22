5e6429ee8e0c8681206a3585e36710f9423a82d3
jakubiakm <maciej190509@gmail.com>
2018-12-22 23:39:33 +0100

    przekazywanie liści do kernela

--- Checkers.Cuda/kernel.cu
+++ Checkers.Cuda/kernel.cu
@@ -6,38 +6,123 @@
 #include <stdlib.h>
 #include <cuda.h>
 #include <math.h> 
-#include <vector>
-//#include <cutil.h>		// timers
-#include "board.h"
-#include "Move.h"
-#include "MCTS.h"
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 
 #define cConst 2
 
 int N = 0;
 
-Board GetBoardAfterMove(Board board, Move move)
+class Move
 {
-	int *pieces = new int[board.size * board.size];
-	for (int i = 0; i != board.size * board.size; i++)
+public:
+	int beatedPiecesCount;
+	int *beatedPieces;
+	int oldPosition;
+	int newPosition;
+	__device__ __host__ Move();
+	__device__ __host__ Move(int oldPosition, int newPosition, int beatedPiecesCount, int *beatedPieces);
+	__device__ __host__ ~Move();
+};
+
+__device__ __host__ Move::Move(int oldPosition, int newPosition, int beatedPiecesCount, int *beatedPieces) : oldPosition(oldPosition), newPosition(newPosition), beatedPiecesCount(beatedPiecesCount), beatedPieces(beatedPieces)
+{
+}
+
+__device__ __host__ Move::Move()
+{
+}
+
+__device__ __host__ Move::~Move()
+{
+}
+enum Player
+{
+	White, Black
+};
+class Board
+{
+public:
+	int size;
+	int* pieces;
+	Player player;
+	__device__ __host__ Board(int size, int* _pieces, Player player) : size(size), player(player)
 	{
-		pieces[i] = board.pieces[i];
+		pieces = new int[size * size];
+		for (int i = 0; i != size * size; i++)
+		{
+			pieces[i] = _pieces[i];
+		}
+	}
+	__device__ __host__ Board();
+	__device__ __host__ ~Board();
+	__device__ __host__ Move* Board::get_possible_moves(int &moves_count);
+	__device__ __host__ Board Board::get_board_after_move(Move move);
+};
+
+__device__ __host__ Board::Board()
+{
+
+}
+
+__device__ __host__ Board::~Board()
+{
+	//delete[]Pieces;
+}
+
+__device__ __host__ Move* Board::get_possible_moves(int &moves_count)
+{
+	moves_count = 0;
+	Move* possibleMoves = new Move[100];
+	return possibleMoves;
+}
+
+__device__ __host__ Board Board::get_board_after_move(Move move)
+{
+	int *_pieces = new int[size * size];
+	for (int i = 0; i != size * size; i++)
+	{
+		_pieces[i] = pieces[i];
 	}
 	for (int i = 0; i != move.beatedPiecesCount; i++)
 	{
-		pieces[move.beatedPieces[i]] = 0;
+		_pieces[move.beatedPieces[i]] = 0;
 	}
-	pieces[move.newPosition] = pieces[move.oldPosition];
-	pieces[move.oldPosition] = 0;
-	return Board(board.size, pieces, board.player == Player::White ? Player::Black : Player::White);
+	_pieces[move.newPosition] = _pieces[move.oldPosition];
+	_pieces[move.oldPosition] = 0;
+	return Board(size, _pieces, player == Player::White ? Player::Black : Player::White);
 }
+
+class MCTS
+{
+public:
+	Player player;
+	int wins;
+	int simulationsCount;
+	bool visitedInCurrentIteration;
+	Board board;
+	MCTS *parent;
+	std::vector<MCTS *> children;
+	MCTS(MCTS *parent, Board board);
+	void add_child(MCTS *child);
+};
+
+MCTS::MCTS(MCTS *parent, Board board) : parent(parent), board(board), wins(0), simulationsCount(0), visitedInCurrentIteration(false)
+{
+}
+
+void MCTS::add_child(MCTS * child)
+{
+	children.push_back(child);
+}
+
 
 MCTS* GenerateRoot(Board startBoard, int movesCount, Move* possibleMoves)
 {
 	MCTS* root = new MCTS(NULL, startBoard);
 	for (int i = 0; i != movesCount; i++)
 	{
-		MCTS* child = new MCTS(root, GetBoardAfterMove(startBoard, possibleMoves[i]));
+		MCTS* child = new MCTS(root, startBoard.get_board_after_move(possibleMoves[i]));
 		root->add_child(child);
 	}
 	return root;
@@ -71,14 +156,15 @@ MCTS* SelectNode(MCTS *parent)
 	}
 	else
 	{
+		int moves_count = 0;
 		if (leafNode->visitedInCurrentIteration)
 			return NULL;
-		auto moves = leafNode->board.get_possible_moves();
-		if (moves.size() == 0)
+		auto moves = leafNode->board.get_possible_moves(moves_count);
+		if (moves_count == 0)
 			return NULL;
-		for (int i = 0; i != moves.size(); i++)
+		for (int i = 0; i != moves_count; i++)
 		{
-			leafNode->add_child(new MCTS(leafNode, GetBoardAfterMove(leafNode->board, moves[i])));
+			leafNode->add_child(new MCTS(leafNode, leafNode->board.get_board_after_move(moves[i])));
 		}
 
 		return leafNode->children[0];
@@ -95,28 +181,19 @@ void BackpropagateSimulations(MCTS *leaf)
 	}
 }
 
-__global__ void PredictNextMove(Board *board, Move* startingMoves)
+__device__ int RolloutBoard(Board board)
 {
-
+	return 1;
 }
 
-// cuda kernel (internal)
-__global__ void some_calculations(float *a, unsigned int N, unsigned int M)
+__global__ void RolloutGames(Board* rolloutBoardsVector, int* results, int size)
 {
-	unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	if (idx < N)
-	{
-		// note1: no need for shared memory here
-		// note2: global memory access is coalesced
-		//        (no structs, float only used)
+	const long numThreads = blockDim.x * gridDim.x;
+	const long threadID = blockIdx.x * blockDim.x + threadIdx.x;
 
-		// do computations M times on each thread
-		// to extend processor time
-		for (unsigned int i = 0; i < M; i++)
-		{
-			// some easy arithmetics		
-			a[idx] = a[idx] * a[idx] * 0.1 - a[idx] - 10;
-		}
+	for (long long ind = threadID; ind < size; ind += numThreads)
+	{
+		results[ind] = RolloutBoard(rolloutBoardsVector[ind]);
 	}
 }
 
@@ -212,37 +289,41 @@ extern "C" int __declspec(dllexport) __stdcall MakeMoveGpu
 		rolloutVector.push_back(node);
 	}
 
-	Move *moves_d;
-	Board *board_d;
+	Board* boards_to_rollout = new Board[rolloutVector.size()];
+	Board* boards_d;
+	int
+		*results_d,
+		*results = new int[rolloutVector.size()];
 
-	cudaMalloc((void**)&moves_d, possibleMovesCount * sizeof(Move));
-	cudaMalloc((void**)&board_d, sizeof(Board));
-	cudaMemcpy(moves_d, moves, possibleMovesCount * sizeof(Move), cudaMemcpyHostToDevice);
-	cudaMemcpy(board_d, &startBoard, sizeof(Board), cudaMemcpyHostToDevice);
-
-	//alokalcja dynamicznych tablic w klasach
-	int* hostData;
-	cudaMalloc((void**)&hostData, sizeof(int)*startBoard.size);
-	cudaMemcpy(hostData, startBoard.pieces, sizeof(int)*startBoard.size, cudaMemcpyHostToDevice);
-	cudaMemcpy(&(board_d->pieces), &hostData, sizeof(int *), cudaMemcpyHostToDevice);
-
-	//alokalcja dynamicznych tablic w klasach
-	for (int i = 0; i < possibleMovesCount; i++)
+	for (int i = 0; i != rolloutVector.size(); i++)
 	{
-		int* hostData;
-		cudaMalloc((void**)&hostData, sizeof(int)*moves[i].beatedPiecesCount);
-		cudaMemcpy(hostData, moves[i].beatedPieces, sizeof(int)*moves[i].beatedPiecesCount, cudaMemcpyHostToDevice);
-		cudaMemcpy(&(moves_d[i].beatedPieces), &hostData, sizeof(int *), cudaMemcpyHostToDevice);
+		boards_to_rollout[i] = rolloutVector[i]->board;
 	}
 
-	PredictNextMove << <gridSize, blockSize >> > (board_d, moves_d);	// kernel invocation
-	cudaThreadSynchronize();			// by default kernel runs in parallel with CPU code
-	
+	cudaMalloc((void**)&boards_d, rolloutVector.size() * sizeof(Board));
+	cudaMalloc((void**)&results_d, rolloutVector.size() * sizeof(int));
+	cudaMemset(boards_d, 0, rolloutVector.size() * sizeof(int));
+	cudaMemcpy(boards_d, boards_to_rollout, rolloutVector.size() * sizeof(Board), cudaMemcpyHostToDevice);
+
+	//alokalcja dynamicznych tablic w klasach
+	for (int i = 0; i < rolloutVector.size(); i++)
+	{
+		int* hostData;
+		cudaMalloc((void**)&hostData, sizeof(int) * boards_to_rollout[i].size);
+		cudaMemcpy(hostData, boards_to_rollout[i].pieces, sizeof(int) * boards_to_rollout[i].size, cudaMemcpyHostToDevice);
+		cudaMemcpy(&(boards_d[i].pieces), &hostData, sizeof(int*), cudaMemcpyHostToDevice);
+	}
+
+	RolloutGames<< <gridSize, blockSize >> > (boards_d, results_d, rolloutVector.size());
+	cudaThreadSynchronize();
+	cudaMemcpy(results, results_d, sizeof(int) * rolloutVector.size(), cudaMemcpyDeviceToHost);
+
 	cuerr = checkCUDAError("cuda kernel");
 
 	if (!cuerr) cuerr = checkCUDAError("cuda memcpy");
 
-	cudaFree(moves_d);
+	cudaFree(boards_d);
+	cudaFree(results_d);
 	if (!cuerr) cuerr = checkCUDAError("cuda free");
 
 	PRINT_ERRORS = tmp;
